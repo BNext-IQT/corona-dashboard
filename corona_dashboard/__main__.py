ea378b891ff36a41ba0e601fc5a165df8b195890
JJ Ben-Joseph <jbenjoseph@iqt.org>
2020-06-23 00:15:25 -0400

    Move imports into the commands.

--- corona_dashboard/__main__.py
+++ corona_dashboard/__main__.py
@@ -1,18 +1,20 @@
 from fire import Fire
-import corona_dashboard.app as app
-from corona_dashboard.forecast import process_data
 
 class Controller:
     def up(self, debug=False):
         """
         Run the web application and build the cache if needed.
         """
+        import corona_dashboard.app as app
+
         app.main(debug)
     
     def cache(self):
         """
         Just cache the data. It doesn't run the web app.
         """
+        from corona_dashboard.forecast import process_data
+        
         process_data()
 
 
