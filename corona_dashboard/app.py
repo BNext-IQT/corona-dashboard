f3bf2819f58ecc4c89c3459cd0ceb7ead0b72db7
JJ Ben-Joseph <jbenjoseph@iqt.org>
2020-08-05 22:22:22 -0400

    Adjust color threshold.

--- corona_dashboard/app.py
+++ corona_dashboard/app.py
@@ -27,7 +27,7 @@ FORECAST_TIMESTAMP = datetime.fromtimestamp(FORECAST_PATH.stat().st_ctime).strft
 
 _MAP = px.choropleth_mapbox(
         US_COUNTIES, geojson=FIPS_METADATA, locations='fips', color='outbreak_risk',
-        color_continuous_scale='orrd', range_color=(0, 50),
+        color_continuous_scale='orrd', range_color=(0, 40),
         hover_name='location',
         hover_data=['outbreak_risk'],
         mapbox_style='carto-darkmatter', zoom=3.2, opacity=0.5,
